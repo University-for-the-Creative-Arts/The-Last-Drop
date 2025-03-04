b6c5b9adbeec32f74d163d357ca6bbcb24f8f307
Sid Szramkowska <151945560+wszramkowska@users.noreply.github.com>
2025-03-04 12:00:25 +0000

    Squashed commit of the following:
    commit 17b0e1cb544b47ecc6ebaa08e899f9a22ebb78c0
    Author: isyvanenko <124314001+isyvanenko@users.noreply.github.com>
    Date:   Mon Mar 3 12:21:24 2025 +0000
    
        Updating Techical log
    
    commit daeb86816853cc688d941b5e567325bff9c53b70
    Author: isyvanenko <124314001+isyvanenko@users.noreply.github.com>
    Date:   Mon Mar 3 11:12:41 2025 +0000
    
        Update Technical devlog.md
    
    commit de62989d3d25f4cf3ff303cad1901685387471b1
    Author: Bora0Dev <164108863+Bora0Dev@users.noreply.github.com>
    Date:   Wed Feb 26 16:14:13 2025 +0000
    
        SurfaceStateComponent/PhysicalSurfaces/GameMechanics
    
        Physical Surfaces +
        Oil and Sap Dropper+
        Fire Surface Actor+
        Pop Sound effects+
        SurfaceStateCheck+
    
    commit 71c2b691d837dd3051debb2bd3f73c1d708f2cc4
    Merge: 2f803c7 c4efc87
    Author: isyvanenko <124314001+isyvanenko@users.noreply.github.com>
    Date:   Tue Feb 25 17:07:10 2025 +0000
    
        Merge pull request #32 from University-for-the-Creative-Arts/AetherialFox
    
        Implementing the AC to BubbleChar, fixing the BP_BodyChar
    
    commit c4efc87b5425b7f49c51323110ea6138758a41c9
    Author: isyvanenko <124314001+isyvanenko@users.noreply.github.com>
    Date:   Tue Feb 25 17:04:17 2025 +0000
    
        Reapply "Implementing the Actor Components"
    
        This reverts commit 2f803c77d3123aea773814b7cc6ab6f7896ba2ba.
    
    commit 2f803c77d3123aea773814b7cc6ab6f7896ba2ba
    Author: isyvanenko <i.sivanenko2002@gmail.com>
    Date:   Tue Feb 25 16:37:45 2025 +0000
    
        Revert "Implementing the Actor Components"
    
        This reverts commit 93a399a8a8c90ef11df7a23f1621e10f7585ab32.
    
    commit e49f96f19554611b35ab0befe9a98588b2506221
    Author: AetherialFox <2309455@students.ucreative.ac.uk>
    Date:   Tue Feb 25 15:41:49 2025 +0000
    
        Create ActorComponentDocumentation.txt
    
    commit fbc997d2352bd6549f6faa2c1a7d50c87c0aabf4
    Merge: 8dd1afc 93a399a
    Author: AetherialFox <2309455@students.ucreative.ac.uk>
    Date:   Tue Feb 25 15:14:44 2025 +0000
    
        Merge branch 'analysis'
    
    commit 93a399a8a8c90ef11df7a23f1621e10f7585ab32
    Author: AetherialFox <2309455@students.ucreative.ac.uk>
    Date:   Tue Feb 25 15:14:09 2025 +0000
    
        Implementing the Actor Components
    
    commit 8dd1afc7e7c3c56393dbe698cee98030d1c5c2d5
    Author: isyvanenko <124314001+isyvanenko@users.noreply.github.com>
    Date:   Tue Feb 25 15:12:58 2025 +0000
    
        Update BP_Dropper.uasset
    
    commit d2795678daabaf59a7b162c492bb90fe7cbcf03d
    Merge: c37ce94 06f1c49
    Author: SzymonPawluk13 <2302143@students.ucreative.ac.uk>
    Date:   Tue Feb 25 12:42:03 2025 +0000
    
        Merge branch 'main' of https://github.com/University-for-the-Creative-Arts/The-Last-Drop
    
    commit c37ce94b67b50c33d94632b777510d8e47c5192a
    Author: SzymonPawluk13 <2302143@students.ucreative.ac.uk>
    Date:   Tue Feb 25 12:41:22 2025 +0000
    
        Autumn Level
    
    commit 06f1c49d2a84b749df809bbcd386b5832dcc1d26
    Merge: 4230225 ca6c5c0
    Author: isyvanenko <124314001+isyvanenko@users.noreply.github.com>
    Date:   Tue Feb 25 12:29:27 2025 +0000
    
        Jumping Platform (Niagara, Material, BP_SapDrop, BP_SapJumpingPlatfrom)
    
        Sap Jumping Platform (Material, Blueprints, Niagara)
    
    commit ca6c5c032c7eb153e6cc1f72a1f46e40eb89de71
    Author: isyvanenko <i.sivanenko2002@gmail.com>
    Date:   Mon Feb 24 22:41:14 2025 +0000
    
        Sap platform (needs adding comments)
    
    commit 627b86c4e80df4bc806d82ecbd01815f88af184f
    Author: isyvanenko <i.sivanenko2002@gmail.com>
    Date:   Fri Feb 21 15:47:25 2025 +0000
    
        Starting of Sap drop

--- Source/TheLastDrop/Private/ActorComponent/SurfaceStateComponent.cpp
+++ Source/TheLastDrop/Private/ActorComponent/SurfaceStateComponent.cpp
@@ -8,6 +8,7 @@
 #include "Components/CapsuleComponent.h"
 #include "Engine/World.h"
 #include "GameFramework/Actor.h"
+#include "Kismet/GameplayStatics.h"
 
 
 USurfaceStateComponent::USurfaceStateComponent()
@@ -64,14 +65,27 @@ void USurfaceStateComponent::UpdateMeshAndNiagara()
 
 	case EBubbleStates::Fire:
 		StaticMeshComponent->SetMaterial(0, FireMaterial);
+		// Play Fire Sound at Actor's Location
+		if (FireSound)
+		{
+			UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetOwner()->GetActorLocation());
+		}
 		break;
 
 	case EBubbleStates::Oil:
 		StaticMeshComponent->SetMaterial(0, OilMaterial);
+		if (FireSound)
+		{
+			UGameplayStatics::PlaySoundAtLocation(this, OilSound, GetOwner()->GetActorLocation());
+		}
 		break;
 
 	case EBubbleStates::Sap:
 		StaticMeshComponent->SetMaterial(0, SapMaterial);;
+		if (FireSound)
+		{
+			UGameplayStatics::PlaySoundAtLocation(this, SapSound, GetOwner()->GetActorLocation());
+		}
 		break;
 
 	default:
@@ -95,12 +109,12 @@ void USurfaceStateComponent::DetectSurface()
 	FVector BubbleLocation = Capsule->GetComponentLocation() - FVector(0, 0, Capsule->GetScaledCapsuleHalfHeight());
 	float SphereRadious = 50.f;
 
-	DrawDebugSphere(GetWorld(), BubbleLocation, SphereRadious, 12, FColor::Blue, false, 1.0f, 0, 2.0f);
+	//DrawDebugSphere(GetWorld(), BubbleLocation, SphereRadious, 12, FColor::Blue, false, 1.0f, 0, 2.0f);
 	FHitResult HitResult;
 	FCollisionQueryParams TraceParams(FName(TEXT("SurfaceTrace")), true, GetOwner());
-
+	TraceParams.bReturnPhysicalMaterial = true;
 	TraceParams.AddIgnoredActor(GetOwner());
-
+#
 	// Perform a line trace
 	if (GetWorld()->SweepSingleByChannel(HitResult, BubbleLocation, BubbleLocation, FQuat::Identity,ECC_Visibility,FCollisionShape::MakeSphere(SphereRadious), TraceParams))
 	{
@@ -108,6 +122,8 @@ void USurfaceStateComponent::DetectSurface()
 		{
 			TObjectPtr<UPhysicalMaterial> HitMaterial = HitResult.PhysMaterial.Get();
 
+			AActor* HitActor = HitResult.GetActor();
+			
 			// Check the material and change state
 			if (HitMaterial == SapSurface)
 			{
@@ -119,18 +135,20 @@ void USurfaceStateComponent::DetectSurface()
 				ChangeState(EBubbleStates::Fire);
 				UE_LOG(LogTemp, Warning, TEXT("Fire"));
 			}
-			else if (HitMaterial == OilSurface)
+			else if (HitMaterial == OilSurface && BubbleStates != EBubbleStates::Fire)
 			{
 				ChangeState(EBubbleStates::Oil);
 				UE_LOG(LogTemp, Warning, TEXT("Oil"));
 			}
-			
+
+			if (!HitActor) return;
+			if (BubbleStates == EBubbleStates::Fire && HitMaterial == FlammableSurface)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("Flammable object hit! Destroying..."));
+				HitActor->Destroy();
+			}
 		}
-		else
-		{
-			UE_LOG(LogTemp, Warning, TEXT("Line trace did not hit any surface."));
-			ChangeState(EBubbleStates::Fire);
-		}
+	
 
 	}
 }
